87d86b86fe4acee8ba0b5f039181388d0b78db8c
Samuel Legrain <legrain.s@outlook.com>
2022-08-19 16:46:09 +0200

    Add correction Indexeur

---
+++ I3WAD22-OO-Exo_banque/Banque.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I3WAD22_OO_Exo_banque
+{
+    class Banque
+    {
+        private Dictionary<string, Courant> _comptes = new Dictionary<string, Courant>();
+        public string Nom { get; set; }
+
+        public Courant this[string numero]
+        {
+            get {
+                //Courant c = null;
+                _comptes.TryGetValue(numero, out Courant c);
+                return c;
+            }
+        }
+
+        public void Ajouter(Courant compte) {
+            if(compte != null && !_comptes.ContainsKey(compte.Numero)) _comptes.Add(compte.Numero, compte);
+        }
+        public void Supprimer(string numero) {
+            if(numero != null && _comptes.ContainsKey(numero)) _comptes.Remove(numero);
+        }
+
+    }
+}
