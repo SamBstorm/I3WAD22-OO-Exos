e5cebc4654d277a5e14f56443477bc250fd4a152
BStorm <BStorm@DESKTOP-LGURCCO>
2022-08-19 09:54:40 +0200

    Ajoutez des fichiers projet.

---
+++ I3WAD22-OO-Exo_banque/Personne.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I3WAD22_OO_Exo_banque
+{
+    class Personne
+    {
+
+        #region Champs - Variables membres
+        private string _nom;
+        private string _prenom;
+        //private DateTime _dateNaissance;
+        #endregion
+
+        #region Propriétés
+        public string Nom
+        {
+            get { return _nom; }
+            set { 
+                if(!string.IsNullOrWhiteSpace(value)) _nom = value.Trim();
+            }
+        }
+
+        public string Prenom
+        {
+            get { return _prenom; }
+            set {
+                if (!string.IsNullOrWhiteSpace(value)) _prenom = value.Trim();
+            }
+        }
+
+        //public DateTime DateNaissance {
+        //    get { return _dateNaissance; }
+        //    set { _dateNaissance = value; }
+        //}
+
+        public DateTime DateNaissance { get; set; }
+        #endregion
+
+        #region Constructeurs & destructeur
+
+        #endregion
+
+        #region Méthodes et opérateurs
+
+        #endregion
+    }
+}
---
+++ I3WAD22-OO-Exo_banque/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace I3WAD22_OO_Exo_banque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personne titulaire1 = new Personne();
+            titulaire1.Nom = "Legrain";
+            titulaire1.Prenom = "Samuel";
+            titulaire1.DateNaissance = new DateTime(1987,9,27);
+
+            Personne titulaire2 = new Personne();
+            titulaire2.Nom = "    Willis      ";
+            titulaire2.Prenom = "";
+            titulaire2.DateNaissance = new DateTime(1987, 9, 27);
+        }
+    }
+}
